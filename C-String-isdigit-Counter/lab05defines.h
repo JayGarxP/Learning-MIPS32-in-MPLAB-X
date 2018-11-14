c28bee33a2e29aa83a64ce3f2a23b426e5c21a1c
JP Fallon <JayGarxP@users.noreply.github.com>
2018-11-14 12:02:39 -0800

    digitcount++ working!
    lab5 handout with extra features

---
+++ C-String-isdigit-Counter/lab05defines.h
@@ -0,0 +1,17 @@
+
+#ifndef _LAB_05_DEFINES_H   /* Guard against multiple inclusion */
+#define _LAB_05_DEFINES_H
+
+#define count_total 56(fp)
+#define count_9 52(fp)
+#define count_8 48(fp)
+#define count_7 44(fp)
+#define count_6 40(fp)
+#define count_5 36(fp)
+#define count_4 32(fp)
+#define count_3 28(fp)
+#define count_2 24(fp)
+#define count_1 20(fp)
+#define count_0 16(fp)
+
+#endif //LAB_05_DEFINES_H
